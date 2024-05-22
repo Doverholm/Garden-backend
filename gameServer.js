a1cde751376281f6500b6967308c2f3841d218b9
Martin Doverholm <martindoverholm.person@gmail.com>
2024-05-22 17:27:41 +0200

    fixed implementing multiplayer without lagg

--- gameServer.js
+++ gameServer.js
@@ -1,96 +1,86 @@
-const randomIntFromInterval = require('./utils/util-functions.js');
 
 const GameObject = require("./GameObject.js");
-const Player = require('./Player.js');
 
-const players = {};
-
-const gameState = {
-    gameObjects: {
-        1: new GameObject({
-            id: "gameObject1",
-            pos: {
-                x: 600,
-                y: 400
-            },
-            sprite: {
-                name: "OBJECT-demo",
-                type: "image",
-                animate: false
-            }
-        }),
-        2: new GameObject({
-            id: "gameObject2",
-            pos: {
-                x: 100,
-                y: 300
-            }
-        })
-    },
-    players: {},
-    map: {
-        name: "MAP-garden"
-    }
-}
-
-const imageMetaData2 = new Map([
-    ["BODY-skeleton", "/BODY-skeleton.png"],
+const imageMetaData = new Map([
+    ["BODY-skeleton", "/BODY_skeletonX1.75.png"],
     ["OBJECT-demo", "/OBJECT-demo.png"],
-    ["MAP-garden", "/MAP-garden.png"]
+    ["MAP-garden", "/garden_mapX2.png"]
 ]);
+
+let gameState = null;
+const players = new Map();
+const objects = new Map();
+objects.set(1, new GameObject({
+    id: "gameObject1",
+    pos: {
+        x: 600,
+        y: 400
+    },
+    sprite: {
+        name: "OBJECT-demo",
+        type: "image",
+        animate: false
+    }
+}));
+objects.set(2, new GameObject({
+    id: "gameObject2",
+    pos: {
+        x: 100,
+        y: 300
+    }
+}));
+
 
 const initGameServer = (io) => {
 
     io.on('connection', (socket) => {
-        const imageMetaDataArray = Array.from(imageMetaData2.entries());
-        socket.emit('imageMetaData', imageMetaDataArray);
+        console.log("player connected: ", socket.id);
 
-        const playerId = socket.id;
-        console.log("player connected: ", playerId);
-    
-        gameState.players[playerId] = new Player({
-            id: playerId,
-            pos: {
-                x: 1200,
-                y: 500
-            },
-            sprite: {
-                name: "BODY-skeleton",
-                type: "sheet",
-                animation: {
-                    animating: false,
-                    totalSteps: 9,
-                    totalFacings: 4,
-                    currentStep: 0,
-                    currentFacing: 2,
-                    size: 64
-                }
-            }
+        socket.on('reqImages', () => {
+            const imageMetaDataArray = Array.from(imageMetaData.entries());
+            socket.emit('resImages', imageMetaDataArray);
         });
 
-        socket.on('keysInput', (inputs) => {
-            gameState.players[playerId].inputs = inputs;
+        socket.on('newPlayer', (player) => {
+            players.set(player.id, player);
+            console.log(players);
+
+            const playersArray = Array.from(players.entries());
+            const objectsArray = Array.from(objects.entries());
+            gameState = {
+                players: playersArray,
+                objects: objectsArray,
+                map: "MAP-garden"
+            };
+            if (players.get(player.id)) {
+                socket.emit('newPlayerInitialized');
+                io.emit('playerJoined', player);
+            } else {
+                socket.emit('newPlayerInitFailed');
+            }
+
+        });
+
+        socket.on('reqGameState', () => {
+            if (gameState) {
+                socket.emit('resGameState', gameState);
+            } else {
+                socket.emit('initGameStateFailed');
+            }
+        });
+        
+        socket.on('newPlayerState', (newState) => {
+            io.emit('newPlayerState', newState);
         });
 
         socket.on('disconnect', () => {
-            console.log("player disconnected: ", playerId);
-            delete gameState.players[playerId];
-            io.emit('updateGameState', JSON.stringify(gameState));
+            console.log("player disconnected: ", socket.id);
+            players.delete(socket.id);
+            //boradcast the deleted player
+            io.emit('playerDisconnected', socket.id);
         });
-
-        io.emit('updateGameState', JSON.stringify(gameState));
     });
 
-    setInterval( () => {
-        updateGameState();
-        io.emit('updateGameState', JSON.stringify(gameState));
-    }, 1000 / 30);
-
 }
-
-const updateGameState = () => {
-    Object.values(gameState.players).forEach(player => player.updatePosition());
-}
-
 module.exports = { initGameServer };
 
